93b22721f955500845f5a6eea44623bafd78810b
Carrie Mathieu <carrieburks@gmail.com>
2021-04-17 16:47:50 -0400

    commit tree dc2f96fb542b7c44a98b4d8b153c0051243e21ef

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return {friends: [...state.friends, {
+                name: action.friend.name,
+                hometown: action.friend.hometown,
+                id: action.friend.id
+            }]}
+        case "REMOVE_FRIEND":
+            let friends = []
+            for (let i = 0; i < state.friends.length; i++) {
+                debugger
+                if (state.friends[i].id !== action.id) {
+                    friends.push(state.friends[i])
+                }
+            }
+            return {friends}
+        default:
+            return state
+    }
 }
